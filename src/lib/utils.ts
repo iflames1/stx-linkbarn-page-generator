debc5839cffc6dd44546000bde1eb1c51cba84d4
Flames <isaacomenuche@gmail.com>
2024-11-04 02:43:48 +0100

    chore: install ui component for project setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
+import axios from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fetchData = async (data: string) => {
+  try {
+    const response = await axios.get("data.json");
+    return response.data;
+  } catch (error) {
+    console.error("Erron fetching", data, "=>", error);
+    return null;
+  }
+};
