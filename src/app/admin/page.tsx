fdcda87f40f9f64644a007d70bb1104055b993c9
Flames <isaacomenuche@gmail.com>
2024-11-11 01:22:36 +0100

    fix: add metadata to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,19 @@
 import TokenUpdateForm from "@/components/admin/form";
-//import TokenManagementForm from "@/components/admin/form1";
 import React from "react";
+import { fetchData } from "@/lib/utils";
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await fetchData();
+
+  return {
+    title: data?.tokenName + " - Admin",
+    description: data?.tokenDescription + " - Admin" || "",
+    openGraph: {
+      images: "/token.jpg",
+    },
+  };
+}
 
 export default function page() {
   return (
